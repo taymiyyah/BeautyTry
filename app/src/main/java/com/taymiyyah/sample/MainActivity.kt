65d03b73746a57bd016520669cd67c620d38a66f
asf <asf>
2021-04-23 21:25:51 +0300

    -add sample

--- app/src/main/java/com/taymiyyah/sample/MainActivity.kt
+++ app/src/main/java/com/taymiyyah/sample/MainActivity.kt
@@ -2,10 +2,30 @@ package com.taymiyyah.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.taymiyyah.beautytry.and
+import com.taymiyyah.beautytry.onAlways
+import com.taymiyyah.beautytry.onFailed
+import com.taymiyyah.beautytry.tries
+import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tries {
+            println("tries")
+            throw RuntimeException()
+        }.and {
+            println("and: this block will execute even tries or any and{} throws")
+        }.and {
+            println("and: this block will execute even tries or any and{} throws")
+        }.onFailed {
+            println("onFailed: this block will execute  if any and{} or ties throws $it")
+        }.onAlways {
+            println("onAlways: this block will Always execute")
+            throw RuntimeException()
+            println("all blocks are safe and never crash")
+        }
     }
 }
